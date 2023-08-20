77cabee3aa6de734ae7499cb76f959ec5c79a789
hjun-park <phj0860@gmail.com>
2023-08-20 21:16:47 +0900

    init: tofu-hansol project

---
+++ src/main/java/com/hansol/tofu/TofuApplication.java
@@ -0,0 +1,13 @@
+package com.hansol.tofu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TofuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TofuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hansol/tofu/TofuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.tofu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TofuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
