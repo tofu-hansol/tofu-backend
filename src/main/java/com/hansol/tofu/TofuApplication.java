8aa2224b8b4b58590cb94bcfba90269fd54eff9f
hjun-park <phj0860@gmail.com>
2023-09-10 19:27:09 +0900

    chore: 서버 관련 설정
    1. gitignore 등록
    2. docker compose 배포 시에 프론트도 함께 배포되도록 변경
    3. querydsl gradle 설정
    4. 사용하지 않는 yml 내용 제거

--- src/main/java/com/hansol/tofu/TofuApplication.java
+++ src/main/java/com/hansol/tofu/TofuApplication.java
@@ -2,7 +2,16 @@ package com.hansol.tofu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients
+@EnableConfigurationProperties
+@EnableJpaAuditing
+@ConfigurationPropertiesScan(basePackages = {"com.hansol.tofu.config",
+	"com.hansol.tofu.auth"})
 @SpringBootApplication
 public class TofuApplication {
 
