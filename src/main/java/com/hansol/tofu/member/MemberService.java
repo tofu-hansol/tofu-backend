e28ec60e13a63fae8ec30b945e5e1fffe70937d4
hjun-park <phj0860@gmail.com>
2023-10-17 15:45:51 +0900

    fix: 동호회 권한이 없을 시 한번 더 권한을 조회하도록 수정

--- src/main/java/com/hansol/tofu/member/MemberService.java
+++ src/main/java/com/hansol/tofu/member/MemberService.java
@@ -4,6 +4,7 @@ import com.hansol.tofu.auth.domain.model.CustomUserDetails;
 import com.hansol.tofu.club.domain.dto.ClubAuthorizationDTO;
 import com.hansol.tofu.dept.repository.DeptRepository;
 import com.hansol.tofu.error.BaseException;
+import com.hansol.tofu.global.SecurityUtils;
 import com.hansol.tofu.member.domain.MemberEntity;
 import com.hansol.tofu.member.domain.dto.MemberEditRequestDTO;
 import com.hansol.tofu.member.domain.dto.MemberJoinRequestDTO;
@@ -39,37 +40,13 @@ public class MemberService {
 
     @Transactional(readOnly = true)
     public Optional<MemberEntity> findMemberBy(String email, MemberStatus status) {
-        return memberRepository.findMemberByEmailAndMemberStatus(email, ACTIVATE);
+        return memberRepository.findMemberByEmailAndMemberStatus(email, status);
     }
 
     public Map<Long, ClubAuthorizationDTO> getCurrentMemberClubAuthority() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
         return principal.getClubAuthorizationDTO();
-    }
-
-//    public void refreshClubAuthority() {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        auth.getDetails();
-//        List<GrantedAuthority> updatedAuthorities = new ArrayList<>(auth.getAuthorities());
-//        updatedAuthorities.add(...); //add your role here [e.g., new SimpleGrantedAuthority("ROLE_NEW_ROLE")]
-//
-//        Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), updatedAuthorities);
-//
-//        SecurityContextHolder.getContext().setAuthentication(newAuth);
-//    }
-
-
-    public String getCurrentMemberEmail() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
-        return principal.getUsername();
-    }
-
-    public Long getCurrentMemberId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
-        return principal.getMemberId();
     }
 
     public Long saveMember(MemberJoinRequestDTO memberRequestDTO) {
@@ -84,7 +61,7 @@ public class MemberService {
     }
 
 	public Long editMemberProfile(MemberEditRequestDTO memberEditRequestDTO) {
-        Long memberId = getCurrentMemberId();
+        Long memberId = SecurityUtils.getCurrentUserId();
 		var memberEntity = memberRepository.findById(memberId)
 				.orElseThrow(() -> new BaseException(NOT_FOUND_MEMBER));
 
@@ -97,7 +74,7 @@ public class MemberService {
 	}
 
 	public Long changeMemberProfileImage(MultipartFile profileImage) {
-        Long memberId = getCurrentMemberId();
+        Long memberId = SecurityUtils.getCurrentUserId();
 
 		var memberEntity = memberRepository.findById(memberId)
 			.orElseThrow(() -> new BaseException(NOT_FOUND_MEMBER));
