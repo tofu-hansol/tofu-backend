39deaf7933d9e5fb7edbc27596a2a2a2d1bd48e7
hjun-park <phj0860@gmail.com>
2023-11-09 11:05:57 +0900

    chore: DTO 필드명 변경

--- src/main/java/com/hansol/tofu/applicant/domain/dto/ClubApplicationResponseDTO.java
+++ src/main/java/com/hansol/tofu/applicant/domain/dto/ClubApplicationResponseDTO.java
@@ -33,7 +33,7 @@ public record ClubApplicationResponseDTO(
 			clubScheduleEntity.getTitle(),
 			clubScheduleEntity.getEventAt().withZoneSameInstant(ZoneId.of("Asia/Seoul")).toLocalDateTime(),
 			clubScheduleEntity.getClubScheduleStatus(),
-			clubScheduleEntity.getPlaceName(),
+			clubScheduleEntity.getContent(),
 			clubScheduleEntity.getLatitude(),
 			clubScheduleEntity.getLongitude(),
 			applicants
